fe66a777887908efb3714c2afbef98a0992f5611
Michael Umoize <michaelumoize@gmail.com>
2026-03-22 01:24:46 +0200

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   async headers() {
     return [
       {
         source: "/pdf.worker.min.mjs",
         headers: [{ key: "Content-Type", value: "application/javascript" }],
+      },
+      {
+        source: "/og-image.png",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/png",
+          },
+        ],
       },
       {
         source: "/(.*)",
